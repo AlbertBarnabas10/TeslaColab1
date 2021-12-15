4f0eb7ccfb0dbf6d0ffcf1f8cb105de2f6022ae9
chris260602 <christoperlim20@gmail.com>
2021-12-15 20:20:05 +0700

    Working carousel

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,3 +10,67 @@ function typeWriter() {
   }
 }
 typeWriter();
+window.onscroll = () => {
+  const aboutsection = document
+    .querySelector("#aboutSection")
+    .getBoundingClientRect();
+  console.log(aboutsection);
+  const navigationbar = document.querySelector("nav");
+  if (aboutsection.top <= 0) {
+    navigationbar.style.backgroundColor = "#000000";
+  } else if (aboutsection.top > 0) {
+    navigationbar.style.backgroundColor = "#00000000";
+  }
+};
+
+const db = [
+  {
+    id: 0,
+    img: "./assets/img/1.jpg",
+    title: "Model S",
+    desc: "Model S",
+    link: "models.html",
+  },
+  {
+    id: 1,
+    img: "./assets/img/2.jpg",
+    title: "Model 3",
+    desc: "Model 3",
+    link: "model3.html",
+  },
+  {
+    id: 2,
+    img: "./assets/img/3.jpg",
+    title: "Model X",
+    desc: "Model X",
+    link: "modelx.html",
+  },
+  {
+    id: 3,
+    img: "./assets/img/4.png",
+    title: "Model Y",
+    desc: "Model Y",
+    link: "modely.html",
+  },
+];
+
+const toggleactivecarousel = (imgselected, chosenone, id) => {
+  for (let i = 0; i < imgselected.length; i++) {
+    imgselected[i].classList.remove("active");
+  }
+  chosenone.classList.add("active");
+  const mainimg = (document.getElementById("mainimg").src = db[id].img);
+  const title = (document.getElementById("abouttitle").innerText =
+    db[id].title);
+  const desc = (document.getElementById("aboutdesc").innerText = db[id].desc);
+  const descbutton = (document.getElementById(
+    "aboutbutton"
+  ).href = `./pages/${db[id].link}`);
+};
+
+const imgselected = document.querySelectorAll(".childimg");
+for (let i = 0; i < imgselected.length; i++) {
+  imgselected[i].addEventListener("click", (e) => {
+    toggleactivecarousel(imgselected, imgselected[i], i);
+  });
+}
